13bc753037721ffae8100651728b8b90df143142
1UC1F3R616 <kushchoudhary8@gmail.com>
2021-10-10 15:26:08 +0530

    all done

--- ubuntu_setup/app.py
+++ ubuntu_setup/app.py
@@ -3,6 +3,8 @@
 from selenium import webdriver
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
 browser.get('https://www.amazon.in/')
@@ -40,13 +42,46 @@ search_submit.click()
 sort_by = browser.find_element_by_class_name('a-dropdown-prompt')
 sort_by.click()
 
+browser.save_screenshot("/home/kush/Desktop/github/selenium-fun/ubuntu_setup/images/items_search.png")
+
+sleep(1)
 sort_selection = browser.find_element_by_link_text('Price: High to Low')
 sort_selection.click()
 
 sleep(3) # let results get loaded
 
+# click on item
 search_result = browser.find_element_by_xpath("//div[@data-index='9']//a")
 search_result.click()
+
+sleep(3)
+
+# switch to window 2
+browser.switch_to.window(browser.window_handles[-1]) # use 0 for main tab, this is for last opened tab
+wishlist_button = browser.find_element_by_id('add-to-wishlist-button-submit')
+wishlist_button.click()
+
+sleep(1)
+
+html = browser.find_element_by_tag_name('html')
+for _ in range(3):
+    html.send_keys(Keys.END)
+    sleep(1)
+
+view_wishlist = browser.find_element_by_link_text('View Wish List')
+view_wishlist.click()
+
+sleep(2)
+
+# SignOut
+action = ActionChains(browser)
+first_level_menu = browser.find_element_by_id('nav-link-accountList-nav-line-1')
+action.move_to_element(first_level_menu).perform()
+second_level_menu = browser.find_element_by_xpath('//*[@id="nav-item-signout"]/span')
+action.move_to_element(second_level_menu).perform()
+second_level_menu.click()
+
+browser.quit() # close
 
 # if __name__ == "__main__":
     # signIn()
